f2ca5310551b99f19ab5c314c1bcf5e5633427c3
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Pro.local>
2024-01-14 23:34:45 +0200

    styling

--- js/radarChart.js
+++ js/radarChart.js
@@ -62,7 +62,7 @@ const radarChartDiv = document.getElementById('radarCanvas');
               color: "#007200",
             },
             pointLabels:{
-              color: "9acd32",
+              color: "#9acd32",
             },
             ticks:{
               display: false,
