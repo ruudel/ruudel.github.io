9bfa9ac0aab289b823da3ec669776ed5f9d36ded
Ulla Simone Möldre <ullasimone@Ullas-MacBook-Pro.local>
2024-01-14 23:42:17 +0200

    styling

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ fetch(graphqlEndpoint, {
     const transactionProjects = [];
     const transactionExercises = [];
     const modifiedSkillAmounts = {};
-    const skillOrder = ["go", "html", "css", "js", "game", "front end", "back end","algo"];
+    const skillOrder = ["go", "html", "css", "js", "game", "front_end", "back","algo"];
     const skillAmounts = [];
     let projectsXp = 0;
     let exercisesXp = 0;
